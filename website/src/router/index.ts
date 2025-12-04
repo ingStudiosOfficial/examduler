9c082d019a4d587fab89dc466f078c355343f6b6
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2025-12-04 19:06:40 +0800

    Modify vite dev server and add dashboard route

--- website/src/router/index.ts
+++ website/src/router/index.ts
@@ -1,8 +1,12 @@
 import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router';
 
 import LoginView from '@/views/LoginView.vue';
+import DashboardView from '@/views/DashboardView.vue';
 
-const routes = [{ path: '/login', name: 'login', component: LoginView, meta: { title: 'Login' } }];
+const routes = [
+    { path: '/login', name: 'login', component: LoginView, meta: { title: 'Login' } },
+    { path: '/dashboard', name: 'dashboard', component: DashboardView, meta: { title: 'Dashboard' } },
+];
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
---
+++ website/src/views/DashboardView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <p>u made it to the dashboard!</p>
+</template>
+
+<style scoped>
+
+</style>
