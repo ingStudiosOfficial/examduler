2580005d4d38121d22704712bb9981e3433684da
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2026-02-03 08:05:18 +0800

    add update route (incomplete)

--- server/src/routes/exam.ts
+++ server/src/routes/exam.ts
@@ -1,6 +1,6 @@
 import { Router, type Request, type Response } from 'express';
 import { authenticateToken, verifyRole } from '../middleware/auth.js';
-import { ObjectId } from 'mongodb';
+import { ObjectId, type AnyBulkWriteOperation } from 'mongodb';
 import type { IExam, IExamCreate } from '../interfaces/Exam.js';
 import { validateCreateExamSchema } from '../middleware/validate_schema.js';
 import { assignExamToUsers, parseExamSeating } from '../utils/exam_utils.js';
@@ -92,6 +92,68 @@ examRouter.post('/create/', authenticateToken(), verifyRole('teacher'), validate
     }
 });
 
+examRouter.patch('/update/:id/', authenticateToken(), verifyRole('teacher'), async (req: Request, res: Response) => {
+    if (!req.params.id || !ObjectId.isValid(req.params.id)) {
+        return res.status(400).json({
+            message: 'Exam ID missing or invalid.',
+        });
+    }
+
+    if (!req.user?.id || !ObjectId.isValid(req.user.id)) {
+        return res.status(400).json({
+            message: 'User ID missing or invalid.',
+        });
+    }
+
+    try {
+        const userId = new ObjectId(req.user.id);
+        const examId = new ObjectId(req.params.id);
+        const examToUpdate: IExam = req.body;
+        const examOps: AnyBulkWriteOperation[] = [];
+        const fieldsToUpdate: object[] = [];
+
+        const user = await req.db.collection<IUser>('users').findOne({ _id: userId });
+
+        console.log('User exams:', user?.exams);
+        console.log('Exam ID:', examId);
+
+        if (!user?.exams.map(e => e.toString()).includes(examId.toString())) {
+            return res.status(403).json({
+                message: 'User is forbidden from updating the exam.',
+            });
+        }
+
+        const examination = await req.db.collection<IExam>('exams').findOne({ _id: examId });
+
+        if (!examination) {
+            console.error('Examination not found.');
+            return res.status(404).json({
+                message: 'Examination not found.',
+            });
+        }
+
+        const keys: (keyof IExam)[] = ['name', 'date', 'description'];
+
+        const fieldsToSet: Partial<IExam> = {};
+
+        keys.forEach(key => {
+            if (examToUpdate[key] !== examination[key]) {
+                (fieldsToSet[key] as unknown) = examToUpdate[key];
+            }
+        });
+
+        if (Object.keys(fieldsToSet).length !== 0) {
+            examOps.push({
+                updateOne: {
+                    filter: { _id: examId },
+                    update: { $set: fieldsToSet },
+                },
+            });
+        }
+    }
+});
+
+// TODO: Wrap this in a transaction
 examRouter.delete('/delete/:id/', authenticateToken(), verifyRole('teacher'), async (req: Request, res: Response) => {
     if (!req.params.id || !ObjectId.isValid(req.params.id)) {
         return res.status(400).json({
