9e15e843e7f716631fc06d7f5933fdc884590ca7
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2026-02-08 16:56:30 +0800

    add md confirm and alert dialogs

---
+++ website/src/components/AlertDialog.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import '@material/web/dialog/dialog.js';
+import '@material/web/button/filled-button/js';
+
+interface ComponentProps {
+    title: string;
+    message: string;
+}
+
+const props = defineProps<ComponentProps>();
+
+const emit = defineEmits(['acknowledged']);
+
+function acknowledge() {
+    emit('acknowledged');
+}
+</script>
+
+<template>
+    <md-dialog>
+        <div slot="headline">
+            {{ props.title }}
+        </div>
+        <div slot="content">
+            {{ props.message }}
+        </div>
+        <div slot="actions">
+            <md-filled-button @click="acknowledge()">OK</md-filled-button>
+        </div>
+    </md-dialog>
+</template>
+
+<style scoped>
+</style>
---
+++ website/src/components/ConfirmDialog.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import '@material/web/dialog/dialog.js';
+import '@material/web/button/text-button/js';
+import '@material/web/button/filled-tonal-button.js';
+
+interface ComponentProps {
+    title: string;
+    message: string;
+}
+
+const props = defineProps<ComponentProps>();
+
+const emit = defineEmits(['confirmed']);
+
+function sendResult(confirmed: boolean) {
+    emit('confirmed', confirmed);
+}
+</script>
+
+<template>
+    <md-dialog>
+        <div slot="headline">
+            {{ props.title }}
+        </div>
+        <div slot="content">
+            {{ props.message }}
+        </div>
+        <div slot="actions">
+            <md-filled-button @click="sendResult(true)">OK</md-filled-button>
+            <md-filled-tonal-button @click="sendResult(false)">Cancel</md-filled-tonal-button>
+        </div>
+    </md-dialog>
+</template>
+
+<style scoped>
+</style>
