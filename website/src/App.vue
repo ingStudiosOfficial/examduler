7bf37d16743a5be57ed165409f5f8764362b90ca
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2026-02-15 12:02:24 +0800

    modify to side navbar

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterView, useRoute } from 'vue-router';
 import NavBar from './components/NavBar.vue';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 const route = useRoute();
 
@@ -9,15 +9,38 @@ const routeTitle = computed(() => {
     return route.meta.title || 'Examduler';
 });
 
-console.log(routeTitle);
+const expanded = ref<boolean>(false);
+
+function toggleExpand(expandValue: boolean) {
+    expanded.value = expandValue;
+}
 </script>
 
 <template>
-    <main>
-        <NavBar :title="routeTitle"></NavBar>
-        <RouterView></RouterView>
+    <main :class="expanded ? 'expanded' : ''">
+        <NavBar class="navbar" :title="routeTitle" @expand="toggleExpand"></NavBar>
+        <div class="router-view">
+            <RouterView></RouterView>
+        </div>
     </main>
 </template>
 
 <style scoped>
+main {
+    display: grid;
+    grid-template-columns: 6dvw 94dvw;
+    width: 100%;
+}
+
+main.expanded {
+    grid-template-columns: 20dvw 80dvw;
+}
+
+.navbar {
+    width: 100%;
+}
+
+.router-view {
+    width: 100%;
+}
 </style>
--- website/src/components/NavBar.vue
+++ website/src/components/NavBar.vue
@@ -1,103 +1,65 @@
 <script setup lang="ts">
-import type { RouteTitle } from '@/types/router';
-import { computed, onMounted, onUnmounted, ref } from 'vue';
+import '@material/web/iconbutton/icon-button.js';
+import '@material/web/icon/icon.js';
+import { ref } from 'vue';
 
 interface ComponentProps {
-    title: RouteTitle;
-};
-
-interface NavLink {
-    title: RouteTitle;
-    url: string;
+    title: string;
 }
 
 const props = defineProps<ComponentProps>();
 
-const isScrolled = ref<boolean>(false);
-const allNavLinks: NavLink[] = [
-    {
-        title: 'Dashboard',
-        url: '/dashboard',
-    }
-];
+const emit = defineEmits(['expand']);
 
-const visibleNavLinks = computed(() => {
-    return allNavLinks.filter(l => l.title !== props.title);
-});
+const expanded = ref<boolean>(false);
 
-let timeout: number | null = null;
-
-function handleScroll() {
-    if (timeout) return;
-
-    timeout = window.setTimeout(() => {
-        isScrolled.value = window.scrollY > 10;
-        timeout = null;
-    }, 100);
+function toggleExpand() {
+    expanded.value = !expanded.value;
+    emit('expand', expanded.value);
 }
-
-onMounted(() => {
-    window.addEventListener('scroll', handleScroll);
-});
-
-onUnmounted(() => {
-    window.removeEventListener('scroll', handleScroll);
-});
 </script>
 
 <template>
-    <header :class="`page-header ${isScrolled ? 'scrolled' : ''}`">
-        <img src="/examduler_logo_trans_full.png" class="examduler-logo" />
-        <h1 class="page-title">{{ props.title }}</h1>
-        <a v-for="link in visibleNavLinks" :key="link.title" class="nav-link" :href="link.url">{{ link.title }}</a>
-    </header>
+    <aside class="sidebar">
+        <div class="icon-wrapper">
+            <md-icon-button @click="toggleExpand()">
+                <md-icon>menu</md-icon>
+            </md-icon-button>
+        </div>
+
+        <div class="sub-sidebar">
+            
+        </div>
+    </aside>
 </template>
 
 <style scoped>
-.page-header {
-    width: 100%;
-    max-height: 10%;
-    padding: 10px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 20px;
-    background-color: var(--md-sys-color-surface);
-    color: var(--md-sys-color-on-surface);
+.sidebar {
     position: sticky;
     top: 0;
     left: 0;
-    z-index: 500;
+    height: 100dvh;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start; 
+    padding: 20px 0;
     box-sizing: border-box;
-    transition: background-color 0.5s ease, color 0.5s ease;
-    text-align: center;
-}
-
-.page-header.scrolled {
     background-color: var(--md-sys-color-surface-container);
-    color: var(--md-sys-color-on-surface-container);
+    transition: width 0.3s ease;
 }
 
-.examduler-title {
-    color: var(--md-sys-color-primary);
-    font-size: 1.2rem;
+.icon-wrapper {
+    width: 6dvw; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-shrink: 0;
 }
 
-.examduler-logo {
-    height: 8dvh;
-}
-
-.page-title {
-    margin: 0;
-    font-size: 1.2rem;
-}
-
-.nav-link {
-    text-decoration: none;
-    color: inherit;
-}
-
-.nav-link:hover {
-    text-decoration: underline;
+.sub-sidebar {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
 }
 </style>
