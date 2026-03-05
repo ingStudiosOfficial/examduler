66502d347bb7fbf22f92fe45864b279fff6ab496
Ethan Lee <pluscompanyhqDEPT@gmail.com>
2026-03-05 08:05:50 +0800

    refactor: use web-haptics instead of navigator.vibrate

--- website/src/directives/vibrate.ts
+++ website/src/directives/vibrate.ts
@@ -3,8 +3,8 @@ import type { DirectiveBinding } from 'vue';
 
 export function vibrateDirective(el: Element, binding: DirectiveBinding) {
     return () => {
-        const duration = (binding.value as number[]) || [10];
-        console.log('Vibrating with element, duration:', el, duration);
-        vibrate(duration);
+        const mode = (binding.value as string) || 'light';
+        console.log('Vibrating with element, mode:', el, mode);
+        vibrate(mode);
     };
 }
--- website/src/utils/vibrate.ts
+++ website/src/utils/vibrate.ts
@@ -1,5 +1,12 @@
-export function vibrate(pattern: number[]) {
-    if ('vibrate' in navigator) {
-        navigator.vibrate(pattern);
+import { useWebHaptics } from "web-haptics/vue";
+const { trigger } = useWebHaptics();
+
+export function vibrate(mode: string) {
+    switch (mode) {
+        case 'light':
+            trigger([
+                { duration: 15 },
+            ], { intensity: 0.4 });
+            break;
     }
 }
